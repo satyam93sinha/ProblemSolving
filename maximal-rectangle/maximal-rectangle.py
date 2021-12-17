8bdac728f23a78a35c26ef88c04d8521c16e9d2b
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2021-12-17 12:09:03 +0530

    Time: 432 ms (26.08%), Space: 15.7 MB (10.67%) - LeetHub

--- maximal-rectangle/maximal-rectangle.py
+++ maximal-rectangle/maximal-rectangle.py
@@ -1,60 +1,76 @@
+class NSL:
+    def nearest_small_left(self, array):
+        stack = [] # to store [index, num]
+        nsl = [0 for _ in range(len(array))]
+        
+        for index, num in enumerate(array):
+            while stack and stack[-1][1] >= num:
+                stack.pop()
+            if stack:
+                nsl[index] = stack[-1][0]
+            else:
+                nsl[index] = -1
+            stack.append([index, num])
+        
+        return nsl
+
+class NSR:
+    def nearest_small_right(self, array):
+        stack = [] # to store [index, num]
+        nsr = [0 for _ in range(len(array))]
+        length = len(array) - 1
+        
+        for index, num in enumerate(array[::-1]):
+            while stack and stack[-1][1] >= num:
+                stack.pop()
+            if stack:
+                nsr[length - index] = stack[-1][0]
+            else:
+                nsr[length - index] = len(array)
+            stack.append([length-index, num])
+        
+        return nsr
+
+class Area:
+    def width(self, array):
+        nsl = NSL().nearest_small_left(array)
+        nsr = NSR().nearest_small_right(array)
+        width = []
+        
+        for nsl_, nsr_ in zip(nsl, nsr):
+            width.append(nsr_-nsl_-1)
+        
+        return width
+    
+    def area_of_array(self, array):
+        width = self.width(array)
+        area = []
+        for index in range(len(width)):
+            area.append(width[index] * array[index])
+        
+        return area
+
+class MaxAreaHistogram:
+    def max_area_histogram(self, array):
+        area = Area().area_of_array(array)
+        return max(area)
+
+
+
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        rows, cols = len(matrix), len(matrix[0])
-        histogram = [0 for _ in range(cols)]
-        max_area_matrix = 0
-        for row in range(rows):
-            for col in range(cols):
-                matrix_element = int(matrix[row][col])
-                if matrix_element == 1:
-                    histogram[col] += matrix_element
+        matrix = [[int(matrix[row][col]) for col in range(len(matrix[row]))] for row in range(len(matrix))]
+        # print(matrix)
+        max_area = float('-inf')
+        current_histogram = [0 for _ in range(len(matrix[0]))]
+        
+        for row in matrix:
+            for index, col in enumerate(row):
+                if col == 1:
+                    current_histogram[index] += col
                 else:
-                    histogram[col] = 0
-            max_area_matrix = max(max_area_matrix,
-                                  MaxAreaHistogram().max_area_histogram(histogram))
-        return max_area_matrix
-                    
-        
-
-        
-class MaxAreaHistogram:
-    def max_area_histogram(self, array: List[int]) -> int:
-        left = NearestSmallerLeft().nearest_smaller_left(array)
-        right = NearestSmallerRight().nearest_smaller_right(array)
-        # calculate width
-        width = [right[index]-left[index]-1 for index in range(len(array))]
-        
-        # calculate max_area and return max_area
-        max_area = 0
-        for index in range(len(array)):
-            max_area = max(max_area, width[index]*array[index])
+                    current_histogram[index] = 0
+            max_area = max(max_area,
+                           MaxAreaHistogram().max_area_histogram(current_histogram))
         return max_area
-
-class NearestSmallerLeft:
-    def nearest_smaller_left(self, array: List[int]) -> List[int]:
-        stack = []
-        left = [0 for _ in range(len(array))]
-        for index in range(len(array)):
-            while stack and array[stack[-1]] >= array[index]:
-                stack.pop()
-            if stack:
-                left[index] = stack[-1]
-            else:
-                left[index] = -1
-            stack.append(index)
-        return left
-
-    
-class NearestSmallerRight:
-    def nearest_smaller_right(self, array: List[int]) -> List[int]:
-        stack = []
-        right = [0 for _ in range(len(array))]
-        for index in range(len(array)-1, -1, -1):
-            while stack and array[stack[-1]] >= array[index]:
-                stack.pop()
-            if stack:
-                right[index] = stack[-1]
-            else:
-                right[index] = len(array)
-            stack.append(index)
-        return right
+        
