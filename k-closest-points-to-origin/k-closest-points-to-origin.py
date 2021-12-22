2d5e8bbfd4021451c6c924270fee1c6e4d0fc0e1
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2021-12-22 06:34:11 +0530

    Time: 692 ms (55.13%), Space: 19.9 MB (51.29%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.py
+++ k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,3 +1,37 @@
+"""
+Edge Cases:
+1. closest points found be in decreasing order
+2. closest points found is in ascending order
+3. no closest point found
+4. only one element
+5. k == n
+
+Approaches:
+1. BRUTE FORCE- Time: O(nlogn), Space: O(1)
+Sort the points array in increasing order using list's inbuilt sort method. The sorting is done on math.sqrt((0-x)**2 + (0-y)**2) distances.
+
+2. Using Heap - Time: O(nlogk), Space: O(k)
+Use a max heap to store points basis their distance from origin and pop the max distance point to insert less distance one iff length of heap reaches k
+
+
+"""
+
+from heapq import heappush, heappop
+
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(points, key=lambda distance: math.sqrt((0-distance[0])**2 + (0-distance[1])**2))[:k]
+        heap = []
+        for point in points:
+            x, y = point[0], point[1]
+            distance = math.sqrt(pow(0-x, 2) + pow(0-y, 2))
+            heappush(heap, (-distance, point))
+            if len(heap) > k:
+                heappop(heap)
+        
+        for index, element in enumerate(heap[:k]):
+            heap[index] = element[1]
+        
+        return heap[:k]
+            
+            
