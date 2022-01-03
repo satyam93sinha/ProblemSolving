e45917c2a8510f572e1f4b8155ec5cfe5e6c013b
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-01-03 12:28:58 +0530

    Time: 306 ms (5.05%), Space: 15.7 MB (10.66%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,10 +1,15 @@
-from collections import Counter
+"""
+Intuition: Maintain a candidate and check if that candidate is the one in consideration, if so we increment the count of that candidate being seen as +1 else -1. If the count reaches 0 we can forget about the candidate in consideration and choose current candidate
+"""
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums_frequency = Counter(nums)
-        max_frequency = nums[0]
-        for num, freq in nums_frequency.items():
-            if nums_frequency[max_frequency] < freq:
-                max_frequency = num
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
         
-        return max_frequency
+        return candidate
+        
