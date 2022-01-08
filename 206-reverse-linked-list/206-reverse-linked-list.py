c0dd5dd5f9eebef39556d8d13bab44e4dcd3aadd
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-01-08 13:33:35 +0530

    Time: 60 ms (10.69%), Space: 15.6 MB (46.17%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -0,0 +1,34 @@
+"""
+Edge Cases:
+1. head is empty -> return head
+2. single element -> return head
+3. Even length of linked list
+4. Odd length of linked list
+                            prev
+    1 <- 2 <- 3 <- 4 <- 5 <- 6
+                                curr nxt
+None <- 1 <- 2 <- 3 <- 4 <- 5
+prev
+None <- 1 None
+    curr    nxt
+"""
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        return prev
