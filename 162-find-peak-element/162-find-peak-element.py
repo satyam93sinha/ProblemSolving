bd47dc45960e8ec5bdbbd64d08034ec3847be5f3
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-01-14 08:29:56 +0530

    Time: 95 ms (5.22%), Space: 14.6 MB (10.98%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.py
@@ -0,0 +1,39 @@
+"""
+Edge Cases:
+1. nums array is in ascending order; return last element as peak element
+2. nums array is in descending order; return first element as peak element
+3. Normal case: Array is not sorted
+    i) There are multiple peak elements, return any of them
+    ii) There is single peak element, return it
+4. Single length nums array -> return the only element as peak element
+
+Approaches:
+1. Brute Force
+Time: O(n) iterate through the array and find the element which is greater than its neighbours
+Space: O(1)
+
+2. Use Binary Search
+Time: O(logn)
+Space: O(1)
+Intuition: find a way to shift left or right and return the peak element if it satisfies the condition of being peak element
+"""
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        nums.append(float('-inf'))
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            # check if mid element is a peak element
+            if nums[mid-1] < nums[mid] > nums[mid+1]:
+                return mid
+            # mid element is not a peak element then, 
+            # check which of the neighbors do not fulfill the condition
+            # for mid to be peak and go towards that half of the array
+            elif nums[mid-1] > nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        
