ce2646f22e156d1f90d582b71046030226b96b0c
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-01-21 07:12:43 +0530

    Time: 88 ms (5.47%), Space: 14.8 MB (34.99%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -14,13 +14,35 @@ Space: O(1)
 Time: O(m + n), ignore the search space/row which can not contain the target by checking if target is in range of row's start column value and row's end column value. If it is not, change the row. When row is found, check every element of row for target
 Space: O(1)
 
-3. Use Binary Search
+3. Use Binary + Linear Search
 Time: O(m + logn), find the row that may contain target and apply binary search to find the target (use property array is sorted row-wise, column-wise and last column to first column of next row)
+Space: O(1)
+
+4. Use Binary Search
+Time: O(logm + logn), first search for correct row(use property columns are sorted and elements are also sorted in matrix) then, search for element in row found using simple binary search, on columns.
 Space: O(1)
 """
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Approach4: Time-O(logm + logn), Space: O(1)
+        row_start, row_end = 0, len(matrix)-1
+        while row_start <= row_end:
+            mid_row = (row_start + row_end) // 2
+            if matrix[mid_row][0] <= target <= matrix[mid_row][-1]:
+                # we found row containing target, search in this row/array
+                return self.binary_search(matrix[mid_row], target)
+            elif target < matrix[mid_row][0]:
+                # target is less than first element of matrix[mid_row]
+                row_end = mid_row - 1
+            else:
+                # target is greater than last element of matrix[mid_row]
+                row_start = mid_row + 1
+        # target not found, row_start > row_end
+        return False
+        
+        """
+        # Approach3: Time: O(m + logn)
         row, col = 0, len(matrix[0])-1
         while 0 <= row < len(matrix) and 0 <= col < len(matrix[0]):
             if matrix[row][0] <= target <= matrix[row][col]:
@@ -30,7 +52,8 @@ class Solution:
             else:
                 break
         return False
-    
+        """
+        
     def binary_search(self, array, target):
         left, right = 0, len(array)-1
         while left <= right:
