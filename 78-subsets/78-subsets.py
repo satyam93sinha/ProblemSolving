5922f9a882c4651556edef88162bc3b7935ff492
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-01-25 08:58:17 +0530

    Time: 66 ms (8.46%), Space: 14.2 MB (92.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,4 +1,6 @@
 """
+Recursive Tree using I/P-O/P method:
+
                             [1, 2, 3]
                                 |
                    -------------------------
@@ -12,6 +14,22 @@
   ----        -----          -------          ----------
 |     |     |       |       |       |        |           |
 {}   [3]  [2]      [2,3]   [1]   [1,3]    [1,2]     [1,2,3]
+
+
+Edge Cases:
+1. Empty nums array : handle in base case
+2. nums array containing single element
+3. nums array containing duplicate elements; a constraint that nums array does not contain any duplicate element
+
+Intuition:
+Generate all the powersets and keep appending them to the output list.
+
+How to generate the powersets?
+-> We can use recursion seeing we have two choices, one to include the element in current_output being considered and the other to exclude it. Using i/p-o/p method we can design the Recursion Tree as designed above to figure out the subsets we need.
+
+To generate all the subsets:
+Time: O(2^n)
+Space: O(2^n) bcoz the output array/list contains at most 2^n elements
 
 """
 
