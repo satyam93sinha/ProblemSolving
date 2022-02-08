4ac93734757bafcb5a33b78e1451308a255e6d10
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-02-08 06:17:56 +0530

    Time: 53 ms (28.21%), Space: 13.8 MB (91.66%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -7,10 +7,31 @@ Approaches:
 1. Brute Force
 Intuition:
 Add the num given and store/change the num itself with the sum of given num. Keep doing this until we get a single digit number.
+Time: O(logn)
+Space: O(1)
+
+2. Mathematical : Digital Root
+Intuition:
+Search for digital root formula.
+To compute a digital root in a decimal numeral system,
+digital_root(base10)(n) = 0, if n == 0
+digital_root(base10) (n) = 9, if n == 9k, number n is divisible by 9
+digital_root(base10) (n) = n mod 9, if n != 9k, number n is not divisible by 9.
+
+Time: O(1)
+Space: O(1)
 """
 
 class Solution:
     def addDigits(self, num: int) -> int:
+        if num == 0:
+            return num
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+        
+        """
         while True:
             curr_sum = 0
             while num > 0:
@@ -19,3 +40,4 @@ class Solution:
             num = curr_sum
             if num < 10:
                 return num
+        """
