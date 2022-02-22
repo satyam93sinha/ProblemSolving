427f6a40ff90cd00de028f4af1fb32e47761f7db
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-02-22 06:38:58 +0530

    Time: 36 ms, Memory: 13.9 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -1,3 +1,18 @@
+"""
+Test Cases:
+[3,2,2,3]
+3
+[0,1,2,2,3,0,4,2]
+2
+[0,1,2,3]
+4
+[1]
+1
+[1,1,1,1,1]
+1
+[1,1,1,2]
+1
+"""
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         remove_element_index = 0
