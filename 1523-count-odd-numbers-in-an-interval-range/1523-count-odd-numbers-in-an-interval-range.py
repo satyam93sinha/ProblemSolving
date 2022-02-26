33dc3acf8b56ad965d9cf192dfb5d6fd58c6df63
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-02-26 09:34:37 +0530

    Time: 51 ms (22.06%), Space: 13.9 MB (59.56%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -12,5 +12,9 @@ class Solution:
             high = high
         else:
             high = high - 1
-        
+        # high - low gives us the total count of odd numbers 0-indexed,
+        # to get the length, we need to increment it by 1
+        # example: 7 - 3 = 4//2 = 2 => 3, 5, 7 ==> 2 + 1 = 3
+        # example: 9 - 9 = 0//2 = 0 => 9 ==> 0 + 1 = 1
+        # example: 101 - 33 = 68//2 = 34 ==> 34 + 1 = 35
         return (high - low) // 2 + 1
