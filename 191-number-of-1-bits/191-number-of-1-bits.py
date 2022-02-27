48e5f54ce55eab88fa61189c9762ceea4c36e417
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-02-27 08:59:39 +0530

    Time: 41 ms (60.93%), Space: 13.9 MB (54.61%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -0,0 +1,21 @@
+"""
+Edge Cases:
+1. n = 0; return 0
+2. n = 1; return 1
+3. n > 1; return count of 1s in binary representation of n
+
+Approaches:
+1. Brute Force
+Intuition:
+Use inbuilt binary representation of a number and convert number to its bin. Iterate over the bin and count total number of 1s.
+Time: O(bin(n))
+Space: O(bin(n))
+"""
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        n = bin(n)[2:]
+        count_1s = 0
+        for char in n:
+            if char == '1':
+                count_1s += 1
+        return count_1s
