e361d4f6d68e4e1cdcb46c6b714a7ad21d72a090
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-02-27 09:56:41 +0530

    Time: 43 ms (56.26%), Space: 13.8 MB (97.88%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -10,12 +10,36 @@ Intuition:
 Use inbuilt binary representation of a number and convert number to its bin. Iterate over the bin and count total number of 1s.
 Time: O(bin(n)) => O(32) => O(1)
 Space: O(bin(n)) => O(32) => O(1)
+
+2. Bit Manipulation
+Intuition:
+There should be a number with which bit manipulated gives us count of 1s or single 1 in given number n.
+Example:
+n = 11 => 1011
+n-1 = 10 => 1010
+If we observe carefully, we can get count of least significant bit 1 by ANDing n and (n-1).
+Time: O(1) just like above
+Space: O(1) no extra space is used here
 """
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        count_1s = 0
+        while n != 0:
+            count_1s += 1
+            # gets n = n and n-1; 11 and 10; 
+            # 1011 & 1010 => 1010; count = 1
+            # 1010 & 1001 => 1000; count = 2
+            # 1000 & 0111 => 0000; count = 3
+            n &= (n-1)
+        return count_1s
+        
+        
+        
+        """
         n = bin(n)[2:]
         count_1s = 0
         for char in n:
             if char == '1':
                 count_1s += 1
         return count_1s
+        """
