bea994229cc230818a90f4a2f9e84494142749d3
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-03-01 16:35:49 +0530

    Time: 40 ms (88.64%), Space: 14 MB (83.92%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -1,10 +1,11 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        arr.sort()
-        common_difference = arr[1] - arr[0]
-        for index in range(2, len(arr)):
-            if arr[index] - arr[index-1] == common_difference:
-                continue
+        min_num = min(arr)
+        common_difference = (max(arr) - min_num)//(len(arr)-1)
+        arr_set = set(arr)  # O(n) time and space to convert list to set
+        for _ in range(len(arr)):
+            if min_num in arr_set:
+                min_num += common_difference
             else:
                 return False
         return True
