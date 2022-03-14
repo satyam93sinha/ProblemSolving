6a313eaa3d4ad198a434ec62a57fab21106b4c67
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-03-14 23:49:17 +0530

    Time: 54 ms (35.16%), Space: 13.8 MB (99.13%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.py
@@ -0,0 +1,31 @@
+"""
+Edge Cases:
+1. if there is consecutive two dots anywhere in the beginning of the string ; return the complete path removing those two dots else if the dots count is greater than two return the whole path
+"""
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        # Initialize a stack
+        stack = []
+
+        # Split the input string on "/" as the delimiter
+        # and process each portion one by one
+        for portion in path.split("/"):
+
+            # If the current component is a "..", then
+            # we pop an entry from the stack if it's non-empty
+            if portion == "..":
+                if stack:
+                    stack.pop()
+            elif portion == "." or not portion:
+                # A no-op for a "." or an empty string
+                continue
+            else:
+                # Finally, a legitimate directory name, so we add it
+                # to our stack
+                stack.append(portion)
+
+        # Stich together all the directory names together
+        final_str = "/" + "/".join(stack)
+        return final_str
