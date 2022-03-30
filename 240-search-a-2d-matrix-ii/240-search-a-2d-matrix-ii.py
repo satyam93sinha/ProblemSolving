1b3d14560170560174e81b20f51852f4d4c988a0
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-03-30 06:23:29 +0530

    Time: 330 ms (14.00%), Space: 20.4 MB (87.82%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,46 +1,46 @@
 """
 Edge Cases:
-1. Empty matrix; False
-2. Single element in matrix; return target == element in matrix
-3. Matrix of size mxn where number of rows and cols are not equal
+1. 1 x 1 matrix; there is only one element; if it is the target return true else false
+2. empty matrix; return false bcoz no element can be found in empty matrix
+3. matrix contain duplicate values even though it is sorted in ascending order; binary search can be used
+4. not a square matrix; can contain only column/row; it won't matter until we handle IndexError
+5. target is not present in matrix
+
+Test Cases:
+1. [[3]], target = 3, target = 5
+2. [[]], target = 10
+3. [[1, 2, 3, 4]], target = 3, target = 5
+4. [[1], [2], [3]], target = 2, target = 6
 
 Approaches:
-1. Brute Force
-Time: O(m*n) : Iterate over the matrix and compare every element
+1. Brute Force:
+Intuition: check every element of matrix and compare with target, if found return True else False
+Time: O(m*n)
 Space: O(1)
 
-2. Use Binary Search (property matrix is sorted in some way)
+2. Use Binary Search on every row to find if target exists
+Time: O(m*log(n))
+Space: O(1)
+
+3. Not Possible bcoz rows and columns are sorted among themselves, next row may or may not be greater than previous row's last element :: Use Binary Search on rows to find the correct/possible row where target could be found and again use simple binary search to find the target in this row
+Time: O(log(m) + log(n))
+Space: O(1)
+
+4. Start from last element of first row and compare with target, if target is greater than this element we have to search in next row else decrement column to search for its correct location.
 Time: O(m + n)
 Space: O(1)
-Intuition:
-Compare target with element in matrix and somehow reduce the search space and go in the direction of finding the target.
-Algorithm: 
-Ensure row and col does not go Out of Bounds
-a) Initialise row, col = 0, len(matrix[0])-1  # start row and end of col
-b) Compare target with matrix[row][col]
-    i) decrement col -= 1 if target < matrix[row][col]
-    ii) increment row += 1 if target > matrix[row][col]
-    iii) if target == matrix[row][col] ; return True
-c) If target is not found in the matrix ; return False
 """
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row, col = 0, len(matrix[0])-1
-        
-        # handling Out of Bounds
-        while (0 <= row < len(matrix)
-               and 0 <= col < len(matrix[0])):
-            
-            # target is found
-            if target == matrix[row][col]:
-                return True
-            # target is smaller -> search in left area/go left
-            elif target < matrix[row][col]:
-                col -= 1
-            # target is greater -> search in right area/go down or right
+        row_start, row_end = 0, len(matrix)-1
+        col_start, col_end = 0, len(matrix[0])-1
+        while row_start <= row_end and col_end >= col_start:
+            if target > matrix[row_start][col_end]:
+                row_start += 1
+            elif target < matrix[row_start][col_end]:
+                col_end -= 1
             else:
-                row += 1
+                return True
         
-        # target not found in matrix
         return False
