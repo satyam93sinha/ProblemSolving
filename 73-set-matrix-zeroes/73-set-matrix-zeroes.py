f343c9a21275863098c74894cb0e1a4a580237db
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-04-15 05:42:08 +0530

    Time: 143 ms (77.68%), Space: 14.8 MB (18.87%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,16 +3,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        zero_indices = set()
+        zero_row_indices = set()
+        zero_col_indices = set()
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 if matrix[row][col] == 0:
-                    zero_indices.add((row, col))
+                    zero_row_indices.add(row)
+                    zero_col_indices.add(col)
         
-        for row, col in zero_indices:
-            for column in range(0, len(matrix[row])):
-                matrix[row][column] = 0
-            
-            for row_index in range(0, len(matrix)):
-                matrix[row_index][col] = 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if row in zero_row_indices or col in zero_col_indices:
+                    matrix[row][col] = 0
         
