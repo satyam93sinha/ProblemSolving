6d72fb51140618aa98478f8b11469d87b48b9bae
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-04-19 21:07:01 +0530

    Time: 79 ms (79.52%), Space: 14.2 MB (94.87%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        def find_two_swapped(root: TreeNode):
+            nonlocal x, y, pred
+            if root is None:
+                return
+            
+            find_two_swapped(root.left)
+            if pred and root.val < pred.val:
+                y = root
+                # first swap occurence
+                if x is None:
+                    x = pred 
+                # second swap occurence
+                else:
+                    return
+            pred = root
+            find_two_swapped(root.right)
+        
+        x = y = pred = None
+        find_two_swapped(root)
+        x.val, y.val = y.val, x.val
