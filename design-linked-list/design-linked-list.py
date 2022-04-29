9f2771540be648eec1c858d8fbf6c0733de0b2c1
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-04-29 08:12:52 +0530

    Time: 325 ms, Memory: 14.7 MB - LeetHub

--- design-linked-list/design-linked-list.py
+++ design-linked-list/design-linked-list.py
@@ -29,75 +29,55 @@ class MyLinkedList:
 
     def __init__(self):
         self.head = None
-        self.tail = None
-        self.size = -1
-
-    def get(self, index: int) -> int:
-        if index > self.size:
-            return -1
-        elif index == 0: # return head's value
-            return self.head.val
-        elif index == self.size: # return tail's value
-            return self.tail.val
-        else: # traverse and find the node -> return node.val
-            current = self.head
-            while index != 0:
-                current = current.next
-                index -= 1
-            return current.val
-
-    def addAtHead(self, val: int) -> None:
-        if not self.head: # LinkedList is empty
-            self.head = self.tail = LinkedListNode(val)
-        else: # LinkedList has at least one node
-            self.head = LinkedListNode(val, self.head)
-        # increment size/length of linkedlist
-        self.size += 1
+    
+    def get_node(self, index: int) -> LinkedListNode:
+        current = self.head
+        while index != 0 and current:
+            current = current.next
+            index -= 1
         
+        return current
+    
+    def get(self, index: int) -> int:
+        node = self.get_node(index)
+        return node.val if node else -1
+        
+    def addAtHead(self, val: int) -> None:
+        node = LinkedListNode(val, self.head)
+        self.head = node
 
+    def get_tail(self) -> LinkedListNode:
+        current = self.head
+        while current and current.next:
+            current = current.next
+        
+        return current
+    
     def addAtTail(self, val: int) -> None:
-        if not self.tail:  # LinkedList is empty
-            return self.addAtHead(val)
-        else:  # LinkedList is not empty
-            self.tail.next = LinkedListNode(val)
-            self.tail = self.tail.next
-            self.size += 1
-            
-
+        current_tail = self.get_tail()
+        if current_tail:
+            current_tail.next = LinkedListNode(val)
+        else:
+            self.addAtHead(val)
+    
     def addAtIndex(self, index: int, val: int) -> None:
-        if index == 0:  # add at head
+        if index == 0:
             return self.addAtHead(val)
-        elif index == self.size+1:  # index is length of linked list, length is always 1-indexed
-            return self.addAtTail(val)
-        elif index > self.size+1:  # invalid case
-            return
-        else:  # 1 < index < length of linked list
-            prev, current = None, self.head
-            while index != 0:
-                prev = current
-                current = current.next
-                index -= 1
-            prev.next = LinkedListNode(val, current)
-            self.size += 1
-
+        else:
+            get_prev_node = self.get_node(index-1)
+            if get_prev_node:
+                get_prev_node.next = LinkedListNode(val, get_prev_node.next)
+        
+        
     def deleteAtIndex(self, index: int) -> None:
-        if index == 0:  # remove from head
+        if index == 0:
             self.head = self.head.next
-        elif index > self.size:  # index is invalid
-            return
-        else:  # 0 < index <= size of index
-            prev, current = None, self.head
-            current_index = 0
-            while current_index != index:
-                prev = current
-                current = current.next
-                current_index += 1
-            prev.next = current.next
-            if not prev.next:
-                self.tail = prev
-        # decrement length/size of linked list
-        self.size -= 1
+        else:
+            get_prev_node = self.get_node(index-1)
+            if get_prev_node and get_prev_node.next:
+                get_prev_node.next = get_prev_node.next.next
 
+        
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
