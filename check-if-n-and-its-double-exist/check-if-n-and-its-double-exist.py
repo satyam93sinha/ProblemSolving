b9b71a945ddc594d63885d1ffffc669bb2812cf1
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-05-28 12:15:22 +0530

    Time: 56 ms, Memory: 14 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,5 +1,12 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+        hashmap = collections.Counter(arr)
+        if hashmap.get(0, 0) > 1:
+            return True
+        for num, count in hashmap.items():
+            if (hashmap.get(num/2, 0) or hashmap.get(num*2, 0)) and num != 0:
+                return True
+        return False
         hashmap = {}
         for num in arr:
             if hashmap.get(num/2, False) or hashmap.get(num*2, False):
