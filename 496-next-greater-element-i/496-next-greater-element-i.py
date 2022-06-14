6008d22cc4217789cee71c45ebd4cfeaeebe5340
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-06-14 06:10:05 +0530

    Time: 78 ms (55.44%), Space: 14.1 MB (94.86%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,25 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        element_index_dict, next_greater_nums2 = self.next_greater(nums2)
-        output = []
-        for num in nums1:
-            next_greater = next_greater_nums2[element_index_dict[num]]
-            output.append(next_greater)
-        return output
-    
-    def next_greater(self, nums: List[int]) -> (Dict, List[int]):
         stack = []
-        element_index_dict = {}
-        next_greater_element = [0 for _ in range(len(nums))]
-        for index in range(len(nums)-1, -1, -1):
-            # stores element and its index in dictionary
-            element_index_dict[nums[index]] = index
-            while stack and stack[-1] < nums[index]:
+        greater_elements = {}
+        for index in range(len(nums2)-1, -1, -1):
+            while stack and stack[-1] < nums2[index]:
                 stack.pop()
-            if not stack:
-                next_greater_element[index] = -1
-            else:
-                next_greater_element[index] = stack[-1]
-            stack.append(nums[index])
+            greater_elements[nums2[index]] = stack[-1] if stack else -1
+            stack.append(nums2[index])
         
-        return element_index_dict, next_greater_element
+        answer = [greater_elements[num] for num in nums1]
+        
+        return answer
