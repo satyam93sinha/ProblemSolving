518197ad1aa386aabbfbab673145d59766090490
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-06-18 05:42:05 +0530

    Time: 372 ms (88.17%), Space: 28.9 MB (49.31%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -5,8 +5,8 @@ class Solution:
         minimum = math.inf
         for index in range(1, len(arr)):
             current_min = arr[index] - arr[index-1]
-            while stack and current_min < minimum:
-                stack.pop()
+            if stack and current_min < minimum:
+                stack = []
             if current_min <= minimum:
                 stack.append([arr[index-1], arr[index]])
                 minimum = current_min
