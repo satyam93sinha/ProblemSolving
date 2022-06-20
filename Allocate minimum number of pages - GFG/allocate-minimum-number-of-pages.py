c12bfea943fc3b170750766161507973bd3fca0c
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-06-20 18:23:24 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
@@ -1,43 +1,60 @@
+"""
+Problem Statement Observations:
+1. N number of books given, has Ai pages in them
+2. sorted in ascending order -> max pages of books is A[-1]
+3. Allocate contiguous books to M students
+4. 0/1 knapsack kind, can not divide the pages of a book, each book assigned to one student
+5. each student should be allocated at least 1 book
+6. Contiguous order
+
+Edge Cases:
+1. M students > len(A) number of books available to be allocated; return -1
+2. M students == len(A); return max of A
+3. M Students < len(A)
+    3.1 books can be allocated to students or it can not be
+
+Approaches:
+1. Brute Force
+Intuition:
+
+"""
+
+
 class Solution:
     
     #Function to find minimum number of pages.
     def findPages(self,A, N, M):
         #code here
-        
+        # edge case1
+        if M > N:
+            return -1
+        if M == N:
+            return max(A)
         
         left, right = max(A), sum(A)
-        # print("left:{}, right:{}".format(left, right))
-        min_pages = -1
+        result = -1
         while left <= right:
-            mid = (left + right) // 2
-            # print("mid pages:", mid)
-            if self.is_valid(A, N, M-1, mid):
-                min_pages = mid
+            mid = (left + right) >> 1
+            if self.is_valid(A, N, M, mid):
+                result = mid
                 right = mid - 1
-                # print("Valid pages, min_pages:{}, mid:{}".format(min_pages, mid))
             else:
                 left = mid + 1
-            # print("while loop end, left:{}, right:{}".format(left, right))
-        return min_pages
         
-    def is_valid(self, array, size, students, pages):
-        sum_ = 0
-        # print("Is Valid, array:{}, students:{}, pages:{}, sum_:{}".format(
-        #                                     array, students, pages, sum_))
-        for page in array:
-            # print("page:", page)
-            sum_ += page
-            # print("sum_:", sum_)
-            if sum_ > pages:
-                sum_ = page
-                students -= 1
-                # print("sum_ > pages, students:", students)
-            if students < 0:
-                # print("Return False!!")
-                return False
+        return result
+    
+    def is_valid(self, array, length, students, max_):
+        allocated_students = 1
+        pages = 0
+        for index in range(length):
+            if pages + array[index] > max_:
+                allocated_students += 1
+                pages = 0
+            pages += array[index]
+        
+        if allocated_students > students:
+            return False
         return True
-                
-
 
 #{ 
 #  Driver Code Starts
