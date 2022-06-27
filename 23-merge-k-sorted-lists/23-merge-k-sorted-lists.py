799ed9a0ac568ba1efef2403b02bffe8356b7c00
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-06-27 07:33:52 +0530

    Time: 122 ms (80.03%), Space: 17.6 MB (93.26%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,33 +1,72 @@
+"""
+Edge Cases:
+All the linked lists provided are sorted
+1. lists is empty, len(lists) == 0; return None
+2. len(lists) is odd, while using conquer strategy with 2 linked lists, we will have to handle the case where there can be only 1-linked list
+3. len(lists) is even
+
+Approaches:
+1. Brute Force, Use DFS
+Intuition:
+Keep a sorted_linked_list as None and keep merging it with every other linked list present in the lists list/array. We will have to iterate over the whole linked lists multiple times, here, k times.
+Time: O(n x k) => O(n ^ 2)
+Space: O(len(sorted_linked_list))
+
+2. Use DFS with mergesort, divide and conquer strategy
+Intuition:
+Divide the whole lists into pairs of two linked lists, then, merge them. In the first iteration of merging we will have halved the lists array and can proceed in the same manner -> O(logk).
+Time: O(nlogk)
+Space: O(len(lists)) => extra space merged_lists containing the merged lists or conquered ones at a time.
+"""
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+        
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        def merge2lists(list1, list2):
-            current1, current2 = list1, list2
-            sorted_current = dummy = ListNode()
-            while current1 and current2:
-                if current1.val <= current2.val:
-                    sorted_current.next = current1
-                    current1 = current1.next
-                else:
-                    sorted_current.next = current2
-                    current2 = current2.next
-                sorted_current = sorted_current.next
-            if current1:
-                sorted_current.next = current1
-            elif current2:
-                sorted_current.next = current2
+        # use merge sort
+        if not lists:
+            return None
+        # keep merging until we get single sorted linked list
+        while len(lists) > 1:
+            # stores sorted O(logk) lists
+            merged_lists = []
+            # dividing the lists into pairs(group of two linked lists)
+            for index in range(0, len(lists), 2):
+                l1 = lists[index]
+                # handle edge case for odd lengthed lists
+                l2 = lists[index + 1] if index + 1 < len(lists) else None
+                merged_lists.append(self.merge_lists(l1, l2))
             
-            return dummy.next
+            # update lists with merged_lists
+            lists = merged_lists
         
-        sorted_linked_list = None
-        for list_ in lists:
-            # print("list:", list_)
-            sorted_linked_list = merge2lists(sorted_linked_list, list_)
-            # print(sorted_linked_list)
+        return lists[0]
+    
+    def merge_lists(self, l1: ListNode, l2: ListNode) -> Optional[ListNode]:
+        dummy = tail = ListNode()
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
         
-        return sorted_linked_list
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        
+        return dummy.next
