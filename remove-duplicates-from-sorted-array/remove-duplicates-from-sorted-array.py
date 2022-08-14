cd4bc8c19b337b2685efe582fc084b84ad4bd1b8
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-08-14 11:45:53 +0530

    Time: 94 ms, Memory: 15.6 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,13 +1,15 @@
 """
 Edge Cases:
 1. Array does not contain any duplicate element; return length of the array
-2. Array is empty; return length of array/zero
+2. Array is empty; return length of array or zero
 3. Array contains duplicate elements; reorder them and return the length of array which contains only single elements, no duplicates
+4. Array contains only duplicates
 
 Test Cases:
 1. [1, 2, 3, 4, 5]
 2. []
 3. [1, 1, 1, 2, 3, 3]
+4. [1, 1, 1, 1, 1]
 
 Approaches:
 1. Brute Force
@@ -19,29 +21,34 @@ Space: O(n) extra array
 
 2. Two pointer
 Intuition:
-Maintain seen and unseen pointers to track index element which has to be overwritten and iterator variable respectively.
+Maintain seen and unseen pointers to track index element which has to be overwritten and iterator variable respectively. Index right before seen or unseen variables would contain the character or letters already encountered so while iterating over the array, moving unseen variable gives an insight of which to be included and which not.
 Time: O(n)
 Space: O(1) in-place, no extra space will be used
 """
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # Edge case if nums has zero or single element
+        if len(nums) < 2:
+            return len(nums)
+        
+        # Edge case, nums has more than one element, could or couldn't contain duplicates
+        seen = 1
+        for unseen in range(1, len(nums)):
+            if nums[unseen] != nums[seen - 1]:
+                nums[seen] = nums[unseen]
+                seen += 1
+        
+        return seen
+        
+        """
         start = 1
         for end in range(1, len(nums)):
             if nums[end] != nums[end-1]:
                 nums[start] = nums[end]
                 start += 1
         return start
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        """
         
         """
         seen = 1
