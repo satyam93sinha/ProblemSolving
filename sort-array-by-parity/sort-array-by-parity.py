b7031322ecc028735d3f1e0efd2fb5be93e96df4
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2022-08-14 13:05:47 +0530

    Time: 90 ms, Memory: 14.7 MB - LeetHub

--- sort-array-by-parity/sort-array-by-parity.py
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -1,5 +1,26 @@
 class Solution(object):
     def sortArrayByParity(self, A):
+        odd_num_index = len(A)-1
+        even_num_index = 0
+        while even_num_index < odd_num_index:
+            # swap if front has odd num and back has even num
+            # (A[even_num_index] % 2) > (A[odd_num_index] % 2)
+            if A[even_num_index] % 2 == 1 and A[odd_num_index] % 2 == 0:
+                A[even_num_index], A[odd_num_index] = A[odd_num_index], A[even_num_index]
+                even_num_index += 1
+                odd_num_index -= 1
+            
+            # front num is an even number
+            if A[even_num_index] % 2 == 0:
+                even_num_index += 1
+            
+            # back num is an odd number
+            if A[odd_num_index] % 2 == 1:
+                odd_num_index -= 1
+        
+        return A
+        
+        """
         odd_num_index = 0
         for even_num_index, num in enumerate(A):
             if num % 2 == 0:
@@ -7,6 +28,7 @@ class Solution(object):
                 odd_num_index += 1
         
         return A
+        """
         
         """
         i, j = 0, len(A) - 1
