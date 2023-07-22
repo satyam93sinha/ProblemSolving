2ce9245915fc4063dbc7362670aaf460fdca7d31
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2023-07-22 22:18:23 +0530

    Time: 368 ms, Memory: 16.7 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.py
+++ 0max-consecutive-ones/0max-consecutive-ones.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def findMaxConsecutiveOnes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        max_ones = consecutive_ones = 0
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count_ones = 0
+        max_consecutive_ones = 0
         for num in nums:
             if num == 1:
-                consecutive_ones += 1
+                count_ones += 1
             else:
-                consecutive_ones = 0
-            max_ones = max(max_ones, consecutive_ones)
+                count_ones = 0
+            max_consecutive_ones = max(max_consecutive_ones, count_ones)
         
-        return max_ones
+        return max_consecutive_ones
