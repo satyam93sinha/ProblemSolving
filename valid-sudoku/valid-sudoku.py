6244ef8027805232541c1c17f93cb6d750b942f8
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2023-08-04 08:11:00 +0530

    Time: 100 ms, Memory: 16.3 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,28 +1,48 @@
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        sub_box_dict = defaultdict(set)
+        row_hashmap = collections.defaultdict(set)
+        col_hashmap = collections.defaultdict(set)
+        matrix_hashmap = collections.defaultdict(set)
         
         for row in range(9):
-            row_set = set()
-            col_set = set()
             for col in range(9):
-                # row
-                if board[row][col] != ".":
-                    if board[row][col] in row_set:
-                        return False
-                    else:
-                        row_set.add(board[row][col])
-                    
-                    if board[row][col] in sub_box_dict[(row//3, col//3)]:
-                        return False
-                    else:
-                        sub_box_dict[(row//3, col//3)].add(board[row][col])
-                
-                # col
-                if board[col][row] != ".":
-                    if board[col][row] in col_set:
-                        return False
-                    else:
-                        col_set.add(board[col][row])
+                if board[row][col] == '.':
+                    continue
+                if (board[row][col] in row_hashmap[row]
+                   or board[row][col] in col_hashmap[col]
+                   or board[row][col] in matrix_hashmap[(row//3, col//3)]):
+                    return False
+                row_hashmap[row].add(board[row][col])
+                col_hashmap[col].add(board[row][col])
+                matrix_hashmap[(row//3, col//3)].add(board[row][col])
+        
         return True
+# class Solution:
+#     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+#         sub_box_dict = defaultdict(set)
+        
+#         for row in range(9):
+#             row_set = set()
+#             col_set = set()
+#             for col in range(9):
+#                 # row
+#                 if board[row][col] != ".":
+#                     if board[row][col] in row_set:
+#                         return False
+#                     else:
+#                         row_set.add(board[row][col])
+                    
+#                     if board[row][col] in sub_box_dict[(row//3, col//3)]:
+#                         return False
+#                     else:
+#                         sub_box_dict[(row//3, col//3)].add(board[row][col])
+                
+#                 # col
+#                 if board[col][row] != ".":
+#                     if board[col][row] in col_set:
+#                         return False
+#                     else:
+#                         col_set.add(board[col][row])
+#         return True
