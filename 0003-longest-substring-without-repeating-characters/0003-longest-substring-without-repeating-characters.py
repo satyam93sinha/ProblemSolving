82777cd1ce017c54771e226d74b9c71040da1f8f
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2023-08-19 09:22:29 +0530

    Time: 66 ms (72.35%), Space: 16.5 MB (46.71%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,48 @@
+"""
+Edge Cases:
+1. s can be empty; return 1 because there is no repeating char
+2. len(s) > 1:
+    a. s has all the chars repeated, same chars until len(s); return 1
+    b. s has all the distinct chars; return len(s)
+    c. s has mixed; find out length of the longest substring without any repeating char
+
+Test Cases:
+""
+"aaaaaaa"
+"abcdef"
+"abcabcbb"
+"pwwkew"
+
+Approaches:
+1. Brute Force
+Intuition:
+Iterate over string s, keep track of the chars that are already seen/visited(set), if its already seen and appears again, break the inner loop.
+Time: O(n^2)
+Space: O(len(s)) or O(longest unique chars in s)
+
+2. Optimal
+Intuition:
+In the above approach, we revisit the already visited chars of string s, so is there a way to keep their calculations somehow to use in future iterations than re-calculating? Sliding Window
+Time: O(n)
+Space: O(n)
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = end = 0
+        seen = set()
+        length_longest_substring = 0
+        while start <= end and end < len(s):
+            # calculation
+            if s[end] not in seen:
+                seen.add(s[end])
+            else:
+                while s[start] != s[end] and start <= end:
+                    seen.remove(s[start])
+                    start += 1
+                start += 1
+            length_longest_substring = max(length_longest_substring, end-start+1)
+            end += 1
+        
+        return length_longest_substring
