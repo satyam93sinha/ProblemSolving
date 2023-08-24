2147daf29d401ba24d5ffc7870d16471d9bf4748
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2023-08-24 16:34:22 +0530

    Time: 58 ms (58.97%), Space: 17 MB (9.88%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,30 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         left, right = 0, len(nums)-1
-        while left <= right and nums[left] == nums[right]:
-            if nums[left] == target:
-                return True
-            left += 1
-            right -= 1
+        # while left <= right and nums[left] == nums[right]:
+        #     if nums[left] == target:
+        #         return True
+        #     left += 1
+        #     right -= 1
         
         while left <= right:
-                
+            
             mid = (left + right) // 2
-            print(f"before - left:{left}, right:{right}")
             
-            if nums[mid] == target:
-                return True
+            if nums[mid] == target: return True
             
-            elif nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
+            elif nums[left] < nums[mid]:
+                if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
+            elif nums[left] > nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
             else:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            print(f"left:{left}, right:{right}, mid:{mid}")
+                left += 1
         
         return False
