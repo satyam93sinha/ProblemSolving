bd0242e073a9f3629573a229019a11394188c060
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2023-08-25 07:49:54 +0530

    Time: 50 ms (86.93%), Space: 20.3 MB (87.88%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cycle, match_node = self.has_cycle(head)
+        # if not cycle: return match_node
+        curr_node = head
+        while curr_node and match_node and curr_node != match_node:
+            curr_node = curr_node.next
+            match_node = match_node.next
+        return match_node
+    
+    def has_cycle(self, head: Optional[ListNode]) -> (bool, Optional[ListNode]):
+        tortoise = hare = head
+        while hare and hare.next:
+            hare = hare.next.next
+            tortoise = tortoise.next
+            if tortoise == hare: return True, hare
+        
+        return False, None
