71ff9b4e1d43a05500905eff40967be802b0b9db
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2024-03-30 18:24:48 +0530

    Time: 284 ms, Memory: 16.8 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.py
+++ 0max-consecutive-ones/0max-consecutive-ones.py
@@ -1,13 +1,29 @@
+"""
+Edge Cases:
+1. All the elements in the array are 0s; max_consecutive 1s = 0
+2. All the elements in the array are 1s; max_consecutive 1s = length of the array
+3. mixed 0s and 1s
+
+Approaches:
+1. Brute Force:
+Nested for loop, outer one iterates over the elements, when 1 is found, we iterate the inner loop which keeps count of the ones stored in another variable. Lastly, return this variable as answer.
+Time: O(n^2), Space: O(1)
+
+2. Optimised:
+Keep iterating over the nums array, if a 1 is found then increment the consecutive_1s, if a 0 is found, reset this consecutive_1s count to 0. Also, keep storing the max_consecutive_1s in this variable.
+Time: O(n), Space: O(1)
+"""
+
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        current_ones = 0
-        max_consecutive_ones = 0
+        consecutive_1s = 0
+        max_consecutive_1s = 0
+        # iterate over the nums array
         for num in nums:
             if num == 1:
-                current_ones += 1
+                consecutive_1s += 1
             else:
-                current_ones = 0
-            
-            max_consecutive_ones = max(max_consecutive_ones, current_ones)
+                consecutive_1s = 0
+            max_consecutive_1s = max(max_consecutive_1s, consecutive_1s)
         
-        return max_consecutive_ones
+        return max_consecutive_1s
