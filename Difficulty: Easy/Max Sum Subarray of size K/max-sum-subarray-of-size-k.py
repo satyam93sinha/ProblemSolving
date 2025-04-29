2b28f494a91427acc99b75299220b2573a824cbb
Satyam Kumar Sinha <31853966+satyam93sinha@users.noreply.github.com>
2025-04-29 21:18:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.py
@@ -1,37 +1,35 @@
 #User function Template for python3
 class Solution:
-    def maximumSumSubarray (self,K,Arr,N):
+    def maximumSumSubarray (self,arr,k):
         # code here 
+        max_sum = curr_sum = 0
         start = end = 0
-        max_sum = float('-inf')
-        curr_sum = 0
-        
-        while end < N:
-            curr_sum += Arr[end]
-            
-            if end - start + 1 < K:
+
+        while end < len(arr):
+            curr_sum += arr[end]
+            if end - start + 1 < k:
                 end += 1
-            elif end - start + 1 == K:
+            else:
                 max_sum = max(max_sum, curr_sum)
-                curr_sum -= Arr[start]
+                curr_sum -= arr[start]
                 start += 1
                 end += 1
-        
         return max_sum
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        
-        N,K = input().split()
-        N = int(N)
-        K = int(K)
-        Arr = list( map(int,input().strip().split(" ")))
 
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        k = int(input().strip())
         ob = Solution()
-        print(ob.maximumSumSubarray(K,Arr,N))
+        ans = ob.maximumSumSubarray(arr, k)
+        print(ans)
+        tc -= 1
+        print("~")
+
 # } Driver Code Ends
